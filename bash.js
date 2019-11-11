8df8f4846410adc2fe6d6fdedf43477d302d2930
Mika Busante <mika@mikabusante.com>
2019-11-11 12:17:37 -0500

    Wrote pwd function and exported and used it

--- bash.js
+++ bash.js
@@ -1,15 +1,14 @@
+const { pwd } = require("./pwd");
 
-const pwd = require('./pwd')
-console.log(pwd)
+process.stdout.write("prompt > ");
 
-process.stdout.write('prompt > ');
+process.stdin.on("data", data => {
+  const cmd = data.toString().trim();
 
-process.stdin.on('data', (data) =>  {
-    const cmd = data.toString().trim();
-
-    process.stdout.write('You typed: ' + cmd);
-    process.stdout.write('\nprompt > ');
+  if (cmd === "pwd") {
+    pwd(cmd);
+  } else {
+    process.stdout.write("You typed: " + cmd);
+    process.stdout.write("\nprompt > ");
+  }
 });
-
-
-
--- pwd.js
+++ pwd.js
@@ -1,12 +1,16 @@
+function pwd(cmd) {
+  process.stdout.write(module.filename);
+  process.stdout.write("\nprompt > ");
+}
 
+module.exports = { pwd };
 
-module. exports = process.stdin.on('data', (data) =>  {
-    const cmd = data.toString().trim();
-    if (cmd === 'pwd')  {
-        process.stdout.write(module.filename);
-        process.stdout.write('prompt > ');
-        
-    }
-   
-    // 
-});
+// module.exports = process.stdin.on("data", data => {
+//   const cmd = data.toString().trim();
+//   if (cmd === "pwd") {
+//     process.stdout.write(module.filename);
+//     process.stdout.write("prompt > ");
+//   }
+
+//   //
+// });
